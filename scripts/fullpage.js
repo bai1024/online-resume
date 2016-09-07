4938291ced0a42996d377def3e02c437e7364d79
Bai Yan <dingdingbai0424@gmail.com>
2016-09-07 12:04:26 +0800

    修改fullpage.js，修复时间轴功能

--- scripts/fullpage.js
+++ scripts/fullpage.js
@@ -1,6 +1,16 @@
 $(function(){
   $('.page-container').fullpage({
     navigation: true,
-    navigationPosition: 'right'
+    navigationPosition: 'right',
+    afterLoad: function(_, index) {
+      if(index === 3) {
+        $(".timeline li").addClass("in-view")
+      }
+    },
+    onLeave: function(index) {
+      if(index === 3) {
+        $(".timeline li").removeClass("in-view")
+      }
+    },
   });
 });
